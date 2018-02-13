ce6d90d2e5c9e3f4e60f237e2243e2a1f4889cf5
Martynas <martynas.viskontas@gmail.com>
2018-02-13 20:29:39 +0200

    add auth action and reducers

---
+++ src/client/app/actions/auth.tsx
@@ -0,0 +1,23 @@
+import { startLoading, endLoading, showToast } from '../actions';
+import { Toast } from '../reducers';
+import { DIALOG_LOADER_ID } from '../client-utils';
+
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+
+export const loginSuccess = (user) => ({type: LOGIN_SUCCESS, user});
+
+export const loginUser = (credentials = {username: 'admin', password: 'admin'}) => dispatch => {
+  dispatch(startLoading(DIALOG_LOADER_ID));
+
+  const mockedUser = {
+    name: 'Martynas',
+    role: 'admin',
+  };
+
+  setTimeout(() => {
+    dispatch(loginSuccess(mockedUser));
+    dispatch(endLoading(DIALOG_LOADER_ID));
+    dispatch(showToast(Toast.success, 'User logged in successfully'));
+  }, 500);
+};
---
+++ src/client/app/reducers/auth.tsx
@@ -0,0 +1,32 @@
+import { LOGIN_SUCCESS, LOGIN_FAILURE } from '../actions';
+
+export interface IUser {
+  name: string;
+  role: string;
+}
+
+export interface IAuthState {
+  isLoggedIn: boolean;
+  user: IUser;
+}
+
+export const auth = (state: IAuthState = {isLoggedIn: false, user: null}, action) => {
+  switch (action.type) {
+    case LOGIN_SUCCESS: {
+      return {
+        ...state,
+        user: action.user,
+        isLoggedIn: true,
+      };
+    }
+    case LOGIN_FAILURE: {
+      return {
+        ...state,
+        user: null,
+        isLoggedIn: false,
+      };
+    }
+    default:
+      return state;
+  }
+};
