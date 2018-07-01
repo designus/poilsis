9f1b2c682dc2d7467d3c091217a4a574b2bf4be8
Martynas <martynas.viskontas@gmail.com>
2018-07-01 19:16:11 +0200

    cleanup

--- src/client/app/pages/admin/createEditItem/mainInfo/form/form.tsx
+++ src/client/app/pages/admin/createEditItem/mainInfo/form/form.tsx
@@ -1,76 +1,75 @@
 import * as React from 'react';
-
+import { Field, reduxForm } from 'redux-form';
 import {
   CheckboxGroup,
   SelectBox,
-  // TextInput,
-  Button,
+  TextInput,
+  // Button,
 } from '../../../../../components';
-import { IAddItemProps } from '../../../../../pages';
-import {
-  // NAME_LABEL,
-  CITY_LABEL,
-  TYPES_LABEL,
-  // ADDRESS_LABEL,
-  SAVE_LABEL,
-  // NAME_KEY,
-  CITY_KEY,
-  TYPES_KEY,
-  // ADDRESS_KEY,
-  USER_LABEL,
-  USER_KEY,
-} from '../../../../../../../data-strings';
 import { isAdmin } from '../../../../../../../global-utils';
 
-export const Form = (props: IAddItemProps) => {
-  const {showErrors, errors, fields} = props.state;
+const required = value => value ? undefined : 'This field is required';
+const maxLength = max => value => value && value.length > max ? `Field must be ${max} characters or less` : undefined;
+const minLength = min => value => value && value.length < min ? `Field must be ${min} characters or more` : undefined;
+// const number = value => value && isNaN(Number(value)) ? 'Field must be a number' : undefined;
+// const email = value => value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ? 'Invalid email address' : undefined;
+
+const minLength3 = minLength(3);
+const minLength1 = minLength(1);
+const maxLength3 = maxLength(3);
+const maxLength15 = maxLength(15);
+
+const Form = props => {
+  const { handleSubmit, pristine, submitting } = props;
   return (
-    <form onSubmit={props.handleSubmit} autoComplete="off">
-      {/* <TextInput
-        label={NAME_LABEL}
-        value={fields[NAME_KEY]}
-        showErrors={showErrors}
-        errors={errors[NAME_KEY]}
-        onChange={props.handleInputChange(NAME_KEY)}
-        onBlur={props.handleOnBlur}
-      /> */}
-      <SelectBox
-        label={CITY_LABEL}
-        value={fields[CITY_KEY]}
-        onChange={props.handleInputChange(CITY_KEY)}
-        showErrors={showErrors}
-        errors={errors[CITY_KEY]}
+    <form onSubmit={handleSubmit} autoComplete="off">
+      <Field
+        name="name"
+        type="text"
+        component={TextInput}
+        validate={[required, minLength3, maxLength15]}
+        label="Name"
+      />
+      <Field
+        name="city"
+        component={SelectBox}
+        validate={[required]}
+        label="City"
         data={props.citiesMap}
+        dataKey="name"
       />
       {isAdmin(props.userRole) &&
-        <SelectBox
-          label={USER_LABEL}
-          value={fields[USER_KEY]}
-          onChange={props.handleInputChange(USER_KEY)}
-          showErrors={showErrors}
-          errors={errors[USER_KEY]}
+        <Field
+          name="userId"
+          component={SelectBox}
+          validate={[required]}
+          label="User"
           data={props.usersMap}
+          dataKey="name"
         />
       }
-      <CheckboxGroup
-        label={TYPES_LABEL}
-        showErrors={showErrors}
-        errors={errors[TYPES_KEY]}
-        onChange={props.handleCheckboxToggle(TYPES_KEY)}
+      <Field
+        name="types"
+        component={CheckboxGroup}
+        validate={[minLength1, maxLength3]}
+        label="Types"
         data={props.typesMap}
-        checkedItems={fields[TYPES_KEY]}
+        dataKey="name"
       />
-      {/* <TextInput
-        label={ADDRESS_LABEL}
-        value={fields[ADDRESS_KEY]}
-        showErrors={showErrors}
-        errors={errors[ADDRESS_KEY]}
-        onChange={props.handleInputChange(ADDRESS_KEY)}
-        onBlur={props.handleOnBlur}
-      /> */}
-      <Button>
-        {SAVE_LABEL}
-      </Button>
+       <Field
+        name="address"
+        type="text"
+        component={TextInput}
+        validate={[required]}
+        label="Address"
+      />
+      <div>
+        <button type="submit" disabled={pristine || submitting}>
+          Submit
+        </button>
+      </div>
     </form>
   );
 };
+
+export const MainInfoForm = reduxForm({ form: 'MainInfoForm' })(Form) as any;
--- src/client/app/pages/admin/createEditItem/mainInfo/mainInfo.tsx
+++ src/client/app/pages/admin/createEditItem/mainInfo/mainInfo.tsx
@@ -1,53 +1,17 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-// import { debounce } from 'lodash';
 import { updateMainInfo, postItem } from '../../../../actions';
-// import { extendWithForm } from '../../../../components';
 import {
-  // getFormStateWithData,
-  getInitialFormState,
   getBackendErrors,
-  // CONTENT_LOADER_ID,
-  TGenericFormModel,
-  IGenericFormState,
-  getKeyMap,
-  required,
-  minLength,
-  maxLength,
   adminRoutes,
 } from '../../../../client-utils';
-// import { Form } from './form';
-import Form from './form/form2';
-import {
-  ID_LABEL,
-  NAME_LABEL,
-  CITY_LABEL,
-  TYPES_LABEL,
-  ADDRESS_LABEL,
-  MAIN_INFO,
-  USER_LABEL,
-} from '../../../../../../data-strings';
+import { MainInfoForm } from './form';
 import { IMainInfoFields } from '../../../../../../global-utils';
 import Typography from '@material-ui/core/Typography';
 import { IAppState } from '../../../../reducers';
-// import { Field, reduxForm } from 'redux-form';
-
-export type TMainInfoModel = TGenericFormModel<IMainInfoFields>;
-
-export const mainInfoModel: TMainInfoModel = {
-  id: getKeyMap('', ID_LABEL, []),
-  name: getKeyMap('', NAME_LABEL, [required]),
-  city: getKeyMap('', CITY_LABEL, [required]),
-  types: getKeyMap([], TYPES_LABEL, [required, minLength(1, true), maxLength(3, true)]),
-  address: getKeyMap('', ADDRESS_LABEL, [required]),
-  userId: getKeyMap('', USER_LABEL, []),
-};
-
-// const MainInfoForm = extendWithForm(Form);
 
 class MainInfoPageComponent extends React.Component<any, any> {
 
-  state: IGenericFormState<IMainInfoFields> = getInitialFormState(mainInfoModel);
   isCreatePage = !Boolean(this.props.match.params.id);
 
   constructor(props) {
@@ -70,15 +34,10 @@ class MainInfoPageComponent extends React.Component<any, any> {
   }
 
   render() {
-    // console.log('Loaded item', this.props.loadedItem);
-    // const initialState = this.props.loadedItem && this.props.loadedItem.isFullyLoaded
-    //   ? getFormStateWithData(this.props.loadedItem, this.state)
-    //   : this.state;
-
     return (this.props.loadedItem || this.isCreatePage) && (
       <div>
-        <Typography variant="headline">{MAIN_INFO}</Typography>
-        <Form
+        <Typography variant="headline">Main info</Typography>
+        <MainInfoForm
           handleSubmit={this.onItemSubmit}
           citiesMap={this.props.citiesMap}
           typesMap={this.props.typesMap}
@@ -86,17 +45,6 @@ class MainInfoPageComponent extends React.Component<any, any> {
           usersMap={this.props.usersMap}
           initialValues={this.props.loadedItem}
         />
-        {/* <MainInfoForm
-          loaderId={CONTENT_LOADER_ID}
-          showLoadingOverlay={true}
-          onItemSubmit={this.onItemSubmit}
-          initialState={initialState}
-          userRole={this.props.userRole}
-          citiesMap={this.props.citiesMap}
-          typesMap={this.props.typesMap}
-          usersMap={this.props.usersMap}
-          isCreate={this.isCreatePage}
-        /> */}
       </div>
     );
   }
