7e1e0c5112ccfb608a79cdc27115077ec997fe48
Martynas <martynas.viskontas@gmail.com>
2018-08-11 11:23:56 +0200

    create cities page#2

---
+++ src/client/app/pages/admin/cities/cities.tsx
@@ -0,0 +1,125 @@
+import * as React from 'react';
+import { connect } from 'react-redux';
+import Typography from '@material-ui/core/Typography';
+import { IAppState, ICitiesMap, ITypesMap } from 'reducers';
+import { AdminHeader } from 'global-styles';
+import { adminRoutes, CONTENT_LOADER_ID } from 'client-utils';
+import { deleteType } from 'actions';
+import {
+  EnhancedTable,
+  ITableColumn,
+  extendWithLoader,
+  ItemActions,
+  ItemTypesList,
+  DeleteModal,
+  AdminPageActions,
+} from 'components';
+
+const Table = extendWithLoader(EnhancedTable);
+
+interface ICitiesPageParams {
+  citiesMap: ICitiesMap;
+  typesMap: ITypesMap;
+  deleteType: (typeId) => Promise<any>;
+}
+
+class AdminCitiesPageComponent extends React.Component<ICitiesPageParams, any> {
+
+  state = {
+    isDeleteModalOpen: false,
+    deleteId: '',
+  };
+
+  get deleteTypeName() {
+    const city = this.props.citiesMap[this.state.deleteId];
+    return city && city.name;
+  }
+
+  get columns(): ITableColumn[] {
+    return [
+      {
+        title: 'Id',
+        dataProp: 'id',
+      },
+      {
+        title: 'Name',
+        dataProp: 'name',
+      },
+      {
+        title: 'Types',
+        dataProp: 'types',
+        format: (types: string[]) => {
+          return (
+            <ItemTypesList
+              typeIds={types}
+              typesMap={this.props.typesMap}
+            />
+          );
+        },
+      },
+      {
+        title: 'Actions',
+        dataProp: 'id',
+        format: (cityId: string) => {
+          return (
+            <ItemActions
+              editLink={adminRoutes.editType.getLink(cityId)}
+              onDelete={this.openDeleteModal(cityId)}
+            />
+          );
+        },
+      },
+    ];
+  }
+
+  openDeleteModal = (cityId) => () => {
+    this.setState({ isDeleteModalOpen: true, deleteId: cityId });
+  }
+
+  handleModalClose = () => {
+    this.setState({ isDeleteModalOpen: false });
+  }
+
+  handleCityDelete = (cityId) => {
+    // return this.props.deleteType(cityId);
+  }
+
+  render() {
+    return (
+      <div>
+        <AdminHeader>
+          <Typography variant="headline">
+            Cities
+          </Typography>
+          <AdminPageActions createLink={adminRoutes.createType.getLink()} />
+        </AdminHeader>
+        <Table
+          showLoadingOverlay={true}
+          loaderId={CONTENT_LOADER_ID}
+          dataMap={this.props.citiesMap}
+          items={Object.keys(this.props.citiesMap)}
+          columns={this.columns}
+          limit={10}
+        />
+        <DeleteModal
+          itemId={this.state.deleteId}
+          isModalOpen={this.state.isDeleteModalOpen}
+          onClose={this.handleModalClose}
+          onDelete={this.handleCityDelete}
+          itemName={this.deleteTypeName}
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state: IAppState) => ({
+  typesMap: state.types.dataMap,
+  citiesMap: state.cities.dataMap,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  deleteCity: (cityId) => dispatch(deleteType(cityId)),
+});
+
+export const AdminCitiesPage = connect<{}, {}, any>(mapStateToProps, mapDispatchToProps)(AdminCitiesPageComponent);
