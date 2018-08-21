7fea2b4ef017a225ed64fcef48bd57371f504a6c
Martynas <martynas.viskontas@gmail.com>
2018-08-21 20:39:41 +0200

    add type controllers

---
+++ src/server/app/controllers/types.tsx
@@ -0,0 +1,32 @@
+import { Request, Response, NextFunction } from 'express';
+import { ITypeFields } from 'global-utils';
+
+import { TypesModel } from '../model';
+import { sendResponse } from '../server-utils';
+
+const shortId = require('shortid');
+
+export const getAllTypes = (req: Request, res: Response, next: NextFunction) => {
+  TypesModel.find(sendResponse(res, next));
+};
+
+export const addNewType = (req: Request, res: Response, next: NextFunction) => {
+  const type: ITypeFields = req.body;
+  const alias = type.alias || type.name;
+  const newType = { ...type, alias, id: shortId.generate() };
+
+  new TypesModel(newType).save(sendResponse(res, next));
+};
+
+export const updateType = (req: Request, res: Response, next: NextFunction) => {
+  const type: ITypeFields = req.body;
+  const typeId = req.params.typeId;
+
+  TypesModel.findOneAndUpdate({ id: typeId },  { $set: type }, { new: true, runValidators: true },
+    sendResponse(res, next),
+  );
+};
+
+export const deleteType = (req: Request, res: Response, next: NextFunction) => {
+  TypesModel.findOneAndRemove({ id: req.params.typeId }, sendResponse(res, next));
+};
--- src/server/app/routes/types.tsx
+++ src/server/app/routes/types.tsx
@@ -1,36 +1,15 @@
 
-import { Request, Response, NextFunction, Router } from 'express';
-import { ITypeFields } from 'global-utils';
-import { TypesModel } from '../model';
-import { sendResponse } from '../server-utils';
-import { auth } from '../controllers';
+import { Router } from 'express';
+import { auth, getAllTypes, addNewType, updateType, deleteType } from '../controllers';
 
 const router = Router();
-const shortId = require('shortid');
 
 router.route('/')
-  .get((req: Request, res: Response, next: NextFunction) => {
-    TypesModel.find(sendResponse(res, next));
-  })
-  .post(auth.authenticate(), auth.authorize(['admin']), (req: Request, res: Response, next: NextFunction) => {
-    const type: ITypeFields = req.body;
-    const alias = type.alias || type.name;
-    const newType = { ...type, alias, id: shortId.generate() };
-
-    new TypesModel(newType).save(sendResponse(res, next));
-  });
+  .get(getAllTypes)
+  .post(auth.authenticate(), auth.authorize(['admin']), addNewType);
 
 router.route('/type/:typeId')
-  .put(auth.authenticate(), auth.authorize(['admin']), (req: Request, res: Response, next: NextFunction) => {
-    const type: ITypeFields = req.body;
-    const typeId = req.params.typeId;
-
-    TypesModel.findOneAndUpdate({ id: typeId },  { $set: type }, { new: true, runValidators: true },
-      sendResponse(res, next),
-    );
-  })
-  .delete(auth.authenticate(), auth.authorize(['admin']), (req: Request, res: Response, next: NextFunction) => {
-    TypesModel.findOneAndRemove({ id: req.params.typeId }, sendResponse(res, next));
-  });
+  .put(auth.authenticate(), auth.authorize(['admin']), updateType)
+  .delete(auth.authenticate(), auth.authorize(['admin']), deleteType);
 
 export default router;
