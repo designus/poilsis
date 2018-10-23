f2fcd7709da5d27adc451c08bd83a9150ff4e565
Martynas <martynas.viskontas@gmail.com>
2018-10-23 21:02:58 +0200

    add user name to admin header

---
+++ src/client/app/components/userMenu/styles.tsx
@@ -0,0 +1,15 @@
+import { Theme } from '@material-ui/core/styles/createMuiTheme';
+import { createStyles } from '@material-ui/core/styles';
+
+export const styles = (theme: Theme) => createStyles({
+  icon: {
+    fill: 'rgba(255,255,255,.8)',
+    width: '34px',
+    height: '34px',
+  },
+  wrapper: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+});
--- src/client/app/components/userMenu/userMenu.tsx
+++ src/client/app/components/userMenu/userMenu.tsx
@@ -1,19 +1,21 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
 import { withStyles } from '@material-ui/core/styles';
+import { WithStyles } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import AccountIcon from '@material-ui/icons/AccountCircle';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 
-const styles = theme => ({
-  icon: {
-    fill: 'rgba(255,255,255,.8)',
-    width: '34px',
-    height: '34px',
-  },
-});
+import { IAppState } from 'reducers';
+import { styles } from './styles';
 
-class MenuComponent extends React.Component<any, any> {
+interface IMenuComponentProps extends WithStyles<typeof styles> {
+  userName?: string;
+}
+
+class MenuComponent extends React.Component<IMenuComponentProps, any> {
 
   state = {
     dropdownAnchorEl: null,
@@ -29,17 +31,18 @@ class MenuComponent extends React.Component<any, any> {
   }
 
   render() {
-    const {classes} = this.props;
     return (
-      <div>
+      <div className={this.props.classes.wrapper}>
+        <Typography color="inherit" variant="body1" align="right">
+          Hello, {this.props.userName}
+        </Typography>
         <IconButton
           aria-label="More"
           aria-owns="Open right Menu"
           aria-haspopup="true"
           onClick={this.handleMenuOpen}
-          className={classes.menuButtonRight}
         >
-          <AccountIcon className={classes.icon} />
+          <AccountIcon className={this.props.classes.icon} />
         </IconButton>
 
         <Menu
@@ -58,4 +61,10 @@ class MenuComponent extends React.Component<any, any> {
   }
 }
 
-export const UserMenu = withStyles(styles)(MenuComponent) as any;
+const mapStateToProps = (state: IAppState) => ({
+  userName: state.currentUser.details.name,
+});
+
+const connectedComponent = connect<any, any, IMenuComponentProps>(mapStateToProps)(MenuComponent);
+
+export const UserMenu = withStyles(styles)(connectedComponent);
