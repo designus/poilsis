7017893d85d572ff2b1218cbabddc2a71980be6c
Martynas <martynas.viskontas@gmail.com>
2018-11-17 12:35:27 +0100

    add connectedIntlProvider component

---
+++ src/client/app/components/connectedIntlProvider/connectedIntlProvider.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { connect } from 'react-redux';
+import { IntlProvider, addLocaleData } from 'react-intl';
+import * as en from 'react-intl/locale-data/en';
+import * as lt from 'react-intl/locale-data/lt';
+import * as ru from 'react-intl/locale-data/ru';
+
+import { IAppState } from 'reducers';
+
+addLocaleData([...en, ...lt, ...ru]);
+
+interface IConnectedIntlProps {
+  locale?: string;
+}
+
+class ConnectedIntl extends React.Component<IConnectedIntlProps, any> {
+  render() {
+    return (
+      <IntlProvider locale={this.props.locale} key={this.props.locale}>
+        {this.props.children}
+      </IntlProvider>
+    );
+  }
+}
+
+const mapStateToProps = (state: IAppState) => ({
+  locale: state.locale,
+});
+
+export const ConnectedIntlProvider = connect<any, any, {}>(mapStateToProps)(ConnectedIntl);
