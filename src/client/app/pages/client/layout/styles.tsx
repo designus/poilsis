590db7a1332fab01d3aee258dd2d202fb088a416
designus <info@designus.lt>
2019-07-16 20:55:16 +0200

    add logo

---
+++ src/client/app/pages/client/layout/styles.tsx
@@ -0,0 +1,51 @@
+import { Theme } from '@material-ui/core/styles/createMuiTheme';
+import { createStyles } from '@material-ui/core/styles';
+
+export const styles = (theme: Theme) => createStyles({
+  '@global': {
+    html: {
+      background: theme.palette.background.default,
+      WebkitFontSmoothing: 'antialiased',
+      MozOsxFontSmoothing: 'grayscale',
+      boxSizing: 'border-box',
+      height: '100%'
+    },
+    '*, *:before, *:after': {
+      boxSizing: 'inherit'
+    },
+    ':focus': {
+      outline: 'none'
+    },
+    body: {
+      height: '100%',
+      margin: 0
+    },
+    'div[id=app]': {
+      height: '100%'
+    }
+  },
+  appBarTitle: {
+    flex: 1
+  },
+  wrapper: {
+    margin: '0 auto',
+    width: '100%',
+    height: '100%'
+  },
+  toolbar: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    padding: '0 15px'
+  },
+  topMenu: {
+    display: 'flex',
+    flex: 3
+  },
+  logo: {
+    flex: 1,
+    '& > img': {
+      height: '30px',
+      width: 'auto'
+    }
+  }
+});
